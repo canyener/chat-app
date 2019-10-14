a070271aeaab0c912efc445d57121c7d33bf7554
canadaslar <can@adaslar.co.uk>
2019-10-14 21:49:36 +0300

    Adds public directory path and uses that directory with express app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || 3000
 
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
