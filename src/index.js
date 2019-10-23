9394b476cced731ad741592e876b4150dfde1793
canyener <can.yener@hotmail.com.tr>
2019-10-23 22:28:01 +0300

    Refactors code which creates server to take advantage of socket.io library

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const path = require('path')
+const http = require('http')
+
 const express = require('express')
 
 const app = express()
+const server = http.createServer(app)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
