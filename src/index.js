b4dc10ce675a6fcffb7a3344056e7195fa49296b
canyener <can.yener@hotmail.com.tr>
2019-10-23 22:30:22 +0300

    Implements socket.io to web server

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const path = require('path')
 const http = require('http')
 
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
