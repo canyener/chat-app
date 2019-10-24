7dafd406548c4f5e36512202c3333645fad89fd6
canyener <can.yener@hotmail.com.tr>
2019-10-24 20:59:52 +0300

    Adds countUpdated event to understand how to send data from server to client

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,5 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', () => {
+    console.log('The count has been updated!')
+})
--- src/index.js
+++ src/index.js
@@ -13,8 +13,12 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('countUpdated')
 })
 
 server.listen(port, () => {
