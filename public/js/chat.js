62cd90778102bb754dec2fd3c1713d56e13fba64
canyener <can.yener@hotmail.com.tr>
2019-11-03 19:26:50 +0300

    Adds listener for the message event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,8 @@
 const socket = io()
 
+socket.on('message', (message) => {
+    console.log(message)
+})
 // socket.on('countUpdated', (count) => {
 //     console.log('The count has been updated!', count)
 // })
