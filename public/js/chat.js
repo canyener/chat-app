2f2a27a5504dca5a4c56bb9a462d9eaa80fb80f6
canyener <can.yener@hotmail.com.tr>
2019-11-03 19:22:26 +0300

    Comments redundant codes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,9 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
-})
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated!', count)
+// })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    socket.emit('increment')
-})
+// document.querySelector('#increment').addEventListener('click', () => {
+//     socket.emit('increment')
+// })
