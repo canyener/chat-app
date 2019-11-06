6fc95915ce3a879bd5263e3921e5e1294e54e30d
canyener <can.yener@hotmail.com.tr>
2019-11-06 23:56:33 +0300

    Emits sendLocation event to send latitude and longitude to server

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,4 +16,13 @@ document.querySelector('#send-location').addEventListener('click', () => {
     if(!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+
+        socket.emit('sendLocation', location)
+    })
 })
