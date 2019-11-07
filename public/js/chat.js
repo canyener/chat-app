b5f4c1075efdfe2e1487f3588d69bfde04e747f5
canyener <can.yener@hotmail.com.tr>
2019-11-08 00:15:12 +0300

    Adds profanity filter to sendMessage event

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,8 +9,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
     
-    socket.emit('sendMessage', message, (message) => {
-        console.log('The message was delivered!', message)
+    socket.emit('sendMessage', message, (error) => {
+       if(error) {
+           return console.log(error)
+       }
+
+       console.log('Message delivered!')
     })
 })
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -20,8 +21,15 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message, callback) => {
+        
+        const filter = new Filter()
+        
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message) 
-        callback('Delivered!')  
+        callback()  
     })
 
     socket.on('sendLocation', (coords) => {
