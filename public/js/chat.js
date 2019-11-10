03d80a7a55dc49b8fab40e99ff16062c13e163b0
canyener <can.yener@hotmail.com.tr>
2019-11-10 15:52:46 +0300

    Clears the message input when the message is sent and sets focust to input

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,6 +18,8 @@ $messageForm.addEventListener('submit', (e) => {
     
     socket.emit('sendMessage', message, (error) => {
         $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
 
         if(error) {
             return console.log(error)
