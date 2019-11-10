e83a9adab61169d1f4be50f7e959e18e3d1fdce2
canyener <can.yener@hotmail.com.tr>
2019-11-10 16:24:50 +0300

    Renders static template with Mustache library inside message event listener

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,10 +8,12 @@ const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
 //Templates
-const messageTemplate = document.querySelector('#message-template')
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate)
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
