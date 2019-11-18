dae291120b31832f63d70635e42e277626b2bef9
canyener <can.yener@hotmail.com.tr>
2019-11-18 20:27:41 +0300

    Adds addUser function

---
+++ src/utils/users.js
@@ -0,0 +1,29 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+    //Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    //Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    //Check for existing user
+    const existingUser = users.find(user => user.room === room && user.username === username)
+
+    //Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    //Store user
+    const user = { id, username, room }
+    users.push(user)
+    return user
+}
