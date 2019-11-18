da85b6650cd52a73d6fdabaeabd78bd237833dcd
canyener <can.yener@hotmail.com.tr>
2019-11-18 20:33:19 +0300

    Adds removeUser function

--- src/utils/users.js
+++ src/utils/users.js
@@ -27,3 +27,11 @@ const addUser = ({ id, username, room }) => {
     users.push(user)
     return user
 }
+
+const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
