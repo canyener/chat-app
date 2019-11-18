9a03b9cd63638ba405332bcca9d87f8dbd057389
canyener <can.yener@hotmail.com.tr>
2019-11-18 20:44:00 +0300

    Exports all user functions

--- src/utils/users.js
+++ src/utils/users.js
@@ -43,3 +43,10 @@ const getUser = (id) => {
 const getUsersInRoom = (room) => {
     return users.filter(user => user.room === room)
 }
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
