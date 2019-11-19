a8c4a3a9f032f56cbd6bf6a6141f5ae9ff9ee000
canyener <can.yener@hotmail.com.tr>
2019-11-19 22:14:52 +0300

    Adds user methods

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 
-const {generateMessage, generateLocationMessage} = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
