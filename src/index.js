7814501902f2dc3c711bcdd3561da2a4e9959421
canyener <can.yener@hotmail.com.tr>
2019-11-19 22:22:01 +0300

    Adds connected user to user list and refactors join event listener

--- src/index.js
+++ src/index.js
@@ -20,11 +20,20 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    socket.on('join', (options, callback) => {
+
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if(error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
 
         socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        socket.broadcast.to(room).emit('message', generateMessage(`${user.username} has joined!`))
+
+        callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
