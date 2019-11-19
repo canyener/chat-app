a0e85f16d9095da460b9c1c365281cbb4c737d3f
canyener <can.yener@hotmail.com.tr>
2019-11-19 23:57:51 +0300

    Renders sidebar template with data

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages')
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //Options
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
@@ -34,8 +35,12 @@ socket.on('locationMessage', (message) => {
 
 
 socket.on('roomData', ({ room, users }) => {
-    console.log(room)
-    console.log(users)
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
