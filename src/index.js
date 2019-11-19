fca47ca6f777e656418f89c9d8cf79c2e262c200
canyener <can.yener@hotmail.com.tr>
2019-11-19 23:17:12 +0300

    Sets username as Admin for system messages

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
+        socket.emit('message', generateMessage('Admin','Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
 
         callback()
@@ -64,7 +64,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left!`))
         }
     })
 })
