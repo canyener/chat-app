de10a56f726742782484c45fa8193d9b19c4a046
canyener <can.yener@hotmail.com.tr>
2019-11-19 23:47:55 +0300

    Adds event listener for roomData and logs room and users to console

--- public/js/chat.js
+++ public/js/chat.js
@@ -32,6 +32,12 @@ socket.on('locationMessage', (message) => {
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
+
+socket.on('roomData', ({ room, users }) => {
+    console.log(room)
+    console.log(users)
+})
+
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
