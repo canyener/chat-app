e895d6760cc1774fa7dbdf5d2a6356e0d67ca8e6
canyener <can.yener@hotmail.com.tr>
2019-11-19 23:39:21 +0300

    Emits roomData event on join

--- src/index.js
+++ src/index.js
@@ -32,7 +32,11 @@ io.on('connection', (socket) => {
 
         socket.emit('message', generateMessage('Admin','Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
-
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+        
         callback()
     })
 
